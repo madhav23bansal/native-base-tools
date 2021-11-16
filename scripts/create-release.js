938836f95dfc5a0b8bed858dfa121f0d7ae5f199
madhav23bansal <madhavb230100@gmail.com>
2021-11-16 12:00:47 +0530

    Fixed release script

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -16,8 +16,31 @@ let packageJsonVersion;
 let remoteData;
 
 function createRelease() {
-    getVersion();
+    setRemoteDataAndCheckForEnv();
 };
+
+function setRemoteDataAndCheckForEnv() {
+    var ls = spawn('git', ['remote', '-v']);
+
+    ls.stdout.on("data", function (data) {
+        remoteData = JSON.stringify(`${data}`);
+    });
+    ls.stderr.on("data", data => {
+        console.log(`Switched to a new branch: release/${data}`);
+    });
+    ls.on("error", error => {
+        console.log(`error: ${error.message}`);
+    });
+    ls.on("close", code => {
+        if (remoteData.match(/\@([^)]+)\:/).pop() == "github.com") {
+            if (!process.env.GITHUB_PERSONAL_TOKEN) {
+                console.error("Please check GITHUB_PERSONAL_TOKEN in your env file");
+                return;
+            }
+            getVersion();
+        }
+    });
+}
 
 function getVersion() {
     var ls = spawn('sh', [getVersionPath]);
@@ -37,15 +60,21 @@ function getVersion() {
 }
 
 async function promptQuestions() {
-    if (!process.env.GITHUB_PERSONAL_TOKEN) {
-        console.error("Please check GITHUB_PERSONAL_TOKEN in your env file");
-    }
-
-    let options = [
+    let subOptions = [
         { title: 'Patch', value: 'patch' },
         { title: 'Minor', value: 'minor' },
         { title: 'Major', value: 'major' },
-        { title: 'Custom', value: 'custom' },
+    ];
+
+    let options = [
+        { title: 'alpha', value: 'alpha' },
+        { title: 'rc', value: 'rc' },
+        { title: 'exact', value: 'exact' },
+    ];
+
+    let optionsWithoutAlpha = [
+        { title: 'rc', value: 'rc' },
+        { title: 'exact', value: 'exact' },
     ];
 
     let promptsConfig = [{
@@ -56,37 +85,78 @@ async function promptQuestions() {
         initial: 0
     }];
 
-    const response = await prompts(promptsConfig);
-    let choice = response.value;
-    if (!choice) {
-        console.error("Please try again!");
-        return;
-    }
+    let subPromptsConfig = [{
+        type: 'select',
+        name: 'value',
+        message: 'Pick an option',
+        choices: subOptions,
+        initial: 0
+    }];
 
-    createVersion(choice);
-}
+    let configWithoutAlpha = [{
+        type: 'select',
+        name: 'value',
+        message: 'Pick an option',
+        choices: optionsWithoutAlpha,
+        initial: 0
+    }];
 
-async function createVersion(choice) {
-    if (choice == 'patch') {
-        version = parseVersion(2);
-    } else if (choice == 'minor') {
-        version = parseVersion(1);
-    } else if (choice == 'major') {
-        version = parseVersion(0);
-    } else {
-        let versionQuestion = [{
-            type: 'text',
-            name: 'value',
-            message: `Please enter custom version`
-        }];
-        const versionFromPrompt = await prompts(versionQuestion);
-        let versionFromUser = versionFromPrompt.value;
-        if (!versionFromUser) {
-            console.error("Please try again!");
+    // let arg = process.argv.slice(3)[0];
+    let choice;
+    let choice2;
+
+    if (packageJsonVersion.includes('alpha')) {
+        const response = await prompts(promptsConfig);
+        choice = response.value;
+        if (!choice) {
+            console.log("Please try again!");
             return;
         }
-        version = versionFromUser;
+    } else if (packageJsonVersion.includes('rc')) {
+        const response = await prompts(configWithoutAlpha);
+        choice = response.value;
+        if (!choice) {
+            console.log("Please try again");
+            return;
+        }
+    } else {
+        const response = await prompts(promptsConfig);
+        choice = response.value;
+        if (!choice) {
+            console.log("Please try again!");
+            return;
+        }
+        const response2 = await prompts(subPromptsConfig);
+        choice2 = response2.value;
+        if (!choice2) {
+            console.log("Please try again!");
+            return;
+        }
     }
+
+    // options.some(element => {
+    //     if (arg == element.value) {
+    //         choice = element.value;
+    //         return true;
+    //     }
+    // });
+    // if (!choice) {
+    //     if (process.argv.slice(3).length > 0) {
+    //         console.error("Please pass correct argument or choose from options given below");
+    //     }
+    //     const response = await prompts(promptsConfig);
+    //     choice = response.value;
+    // }
+    // if (!choice) {
+    //     console.error("Please try again!");
+    //     return;
+    // }
+
+    createVersion(choice, choice2);
+}
+
+async function createVersion(choice, choice2) {
+    version = parseVersion(choice, choice2);
     releaseBranch = 'release/' + version;
     getCurrentBranch();
 }
@@ -108,7 +178,8 @@ function getCurrentBranch() {
 }
 
 function checkBranch() {
-    if (currentBranch.trim() == 'development' || currentBranch.trim() == "minor" || currentBranch.trim() == "patch") {
+    currentBranch = currentBranch.trim();
+    if (currentBranch == 'development' || currentBranch == "minor" || currentBranch == "patch") {
         bumpVersion();
     } else {
         console.log('\nRelease can only be created from development or minor or patch branch.\nYour current branch is', currentBranch);
@@ -148,20 +219,13 @@ function createReleaseBranch() {
 }
 
 function checkRemote() {
-    var ls = spawn('git', ['remote', '-v']);
-
-    ls.stdout.on("data", function (data) {
-        remoteData = JSON.stringify(`${data}`);
-    });
-    ls.stderr.on("data", data => {
-        console.log(`Switched to a new branch: release/${data}`);
-    });
-    ls.on("error", error => {
-        console.log(`error: ${error.message}`);
-    });
-    ls.on("close", code => {
+    if (remoteData) {
         createPR();
-    });
+    }
+    else {
+        console.error("Remote Data cannot be empty");
+        return;
+    }
 }
 
 function createPR() {
@@ -207,7 +271,6 @@ function createPRforGithub(repo, owner, title, base, head) {
     console.log("Created PR!");
 }
 
-
 function createPRforGitlab() {
     var ls = spawn('git', ['push', '-o', 'merge_request.create', '-o', 'merge_request.target=master', 'origin', 'release/' + version]);
     ls.stdout.on("data", function (data) {
@@ -225,12 +288,67 @@ function createPRforGitlab() {
 }
 
 // Utils
-function parseVersion(index) {
+function parseVersion(choice, choice2) {
+    if (choice == 'exact') {
+        if (packageJsonVersion.includes('alpha') || packageJsonVersion.includes('rc')) {
+            let parsedArray = packageJsonVersion.split('-');
+            return parsedArray[0];
+        }
+        else {
+            if (choice2 == 'patch') {
+                return parseVersionType(2);
+            } else if (choice2 == 'minor') {
+                return parseVersionType(1);
+            } else if (choice2 == 'major') {
+                return parseVersionType(0);
+            }
+        }
+    } else if (choice == 'alpha') {
+        if (packageJsonVersion.includes('alpha')) {
+            let x = packageJsonVersion.split('.');
+            return parseVersionType(x.length - 1);
+        } else {
+            let x;
+            if (choice2 == 'patch') {
+                x = parseVersionType(2);
+            } else if (choice2 == "minor") {
+                x = parseVersionType(1);
+            } else if (choice2 == "major") {
+                x = parseVersionType(0);
+            }
+            x = x + '-alpha.0';
+            return x;
+        }
+    } else if (choice == 'rc') {
+        if (packageJsonVersion.includes('alpha')) {
+            let parsedArray = packageJsonVersion.split('-');
+            parsedArray[parsedArray.length - 1] = 'rc.0';
+            return parsedArray.join('-');
+        }
+        else if (packageJsonVersion.includes('rc')) {
+            let x = packageJsonVersion.split('.');
+            return parseVersionType(x.length - 1);
+        }
+        else {
+            let x;
+            if (choice2 == 'patch') {
+                x = parseVersionType(2);
+            } else if (choice2 == "minor") {
+                x = parseVersionType(1);
+            } else if (choice2 == "major") {
+                x = parseVersionType(0);
+            }
+            x = x + '-rc.0';
+            return x;
+        }
+    }
+}
+
+function parseVersionType(index) {
     let parsedArray = packageJsonVersion.split('.');
     parsedArray[index] = parseFloat(parsedArray[index]) + 1;
     let final = parsedArray.join('.');
     return final;
 }
-
 
 module.exports.createRelease = createRelease;
